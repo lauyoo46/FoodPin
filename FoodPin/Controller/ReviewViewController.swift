f1cc8f8550da22d4b27c0908bce74ed5935e37d1
laurentiuile <ilelaurentiu@yahoo.com>
2020-12-15 18:04:14 +0200

    Refactor Review Controller & add animation to close button

--- FoodPin/Controller/ReviewViewController.swift
+++ FoodPin/Controller/ReviewViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ReviewViewController: UIViewController {
     
+    @IBOutlet var closeButton: UIButton!
     @IBOutlet var rateButtons: [UIButton]!
     @IBOutlet var backgroundImageView: UIImageView!
     var restaurant = Restaurant()
@@ -22,39 +23,34 @@ class ReviewViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         UIView.animate(withDuration: 0.4, delay: 0.1, options: [], animations: {
-            self.rateButtons[0].alpha = 1
-            self.rateButtons[0].transform = .identity
+            self.closeButton.alpha = 1
+            self.closeButton.transform = .identity
         }, completion: nil)
         
-        UIView.animate(withDuration: 0.4, delay: 0.15, options: [], animations: {
-            self.rateButtons[1].alpha = 1
-            self.rateButtons[1].transform = .identity
-        }, completion: nil)
-        
-        UIView.animate(withDuration: 0.4, delay: 0.2, options: [], animations: {
-            self.rateButtons[2].alpha = 1
-            self.rateButtons[2].transform = .identity
-        }, completion: nil)
-        
-        UIView.animate(withDuration: 0.4, delay: 0.25, options: [], animations: {
-            self.rateButtons[3].alpha = 1
-            self.rateButtons[3].transform = .identity
-        }, completion: nil)
-        
-        UIView.animate(withDuration: 0.4, delay: 0.3, options: [], animations: {
-            self.rateButtons[4].alpha = 1
-            self.rateButtons[4].transform = .identity
-        }, completion: nil)
+        var delay = 0.1
+        for i in 0...4 {
+            UIView.animate(withDuration: 0.4, delay: delay, options: [], animations: {
+                self.rateButtons[i].alpha = 1
+                self.rateButtons[i].transform = .identity
+            }, completion: nil)
+            delay += 0.05
+        }
     }
     
     func makeButtonsInvisible() {
-        let moveRightTransform = CGAffineTransform.init(translationX: 600, y: 0)
-        let scaleUpTransform = CGAffineTransform.init(scaleX: 5.0, y: 5.0)
-        let moveScaleTransform = scaleUpTransform.concatenating(moveRightTransform)
+        let moveRightRatingsTransform = CGAffineTransform.init(translationX: 600, y: 0)
+        let scaleUpRatingsTransform = CGAffineTransform.init(scaleX: 5.0, y: 5.0)
+        let moveScaleRatingsTransform = scaleUpRatingsTransform.concatenating(moveRightRatingsTransform)
         for rateButton in rateButtons {
-            rateButton.transform = moveScaleTransform
+            rateButton.transform = moveScaleRatingsTransform
             rateButton.alpha = 0
         }
+        
+        let moveTopCloseTransform = CGAffineTransform.init(translationX: 0, y: -600)
+        let scaleUpCloseTransform = CGAffineTransform.init(scaleX: 5.0, y: 5.0)
+        let moveScaleCloseTransform = scaleUpCloseTransform.concatenating(moveTopCloseTransform)
+        closeButton.alpha = 0
+        closeButton.transform = moveScaleCloseTransform
     }
     
     func blurBackgroundImage() {
