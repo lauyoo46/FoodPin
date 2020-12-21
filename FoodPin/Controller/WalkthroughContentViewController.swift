d5366827cac1a872d7f98cac4d5ee2d2b04da223
laurentiuile <ilelaurentiu@yahoo.com>
2020-12-21 12:44:38 +0200

    Adds tutorial for app as a PageViewController

---
+++ FoodPin/Controller/WalkthroughContentViewController.swift
@@ -0,0 +1,39 @@
+//
+//  WalkthroughContentViewController.swift
+//  FoodPin
+//
+//  Created by Laurentiu Ile on 18/12/2020.
+//
+
+import UIKit
+
+class WalkthroughContentViewController: UIViewController {
+    
+    @IBOutlet var headingLabel: UILabel! {
+        didSet {
+            headingLabel.numberOfLines = 0
+        }
+    }
+    
+    @IBOutlet var subHeadingLabel: UILabel! {
+        didSet {
+            subHeadingLabel.numberOfLines = 0
+        }
+    }
+    
+    @IBOutlet var contentImageView: UIImageView!
+    
+    var index = 0
+    var heading = ""
+    var subHeading = ""
+    var imageFile = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        headingLabel.text = heading
+        subHeadingLabel.text = subHeading
+        contentImageView.image = UIImage(named: imageFile)
+    }
+    
+}
